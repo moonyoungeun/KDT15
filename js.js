9b0ca96aa8eb5a6206cf17e22323826fab99d93b
moonyoungeun <pdu04223@naver.com>
2022-07-22 17:56:07 +0900

    220722

--- js.js
+++ js.js
@@ -64,13 +64,66 @@
 
 // console.log(fore);
 
-const fruits = ["사과", "파인애플", "수박", "포도", "오렌지"];
+// const fruits = ["사과", "파인애플", "수박", "포도", "오렌지"];
 
-let obj = fruits.map(function(item, index){
-    return{
-        id: index+1 ,
-        name : item,
+// let obj = fruits.map(function(item, index){
+//     return{
+//         id: index+1 ,
+//         name : item,
+//     }
+// })
+
+// console.log(obj);
+
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+
+// let reduce = number.reduce(function(sum, item, index, arr){
+//     console.log(sum, item, index, arr);
+//     return sum + item;
+// })
+
+// let numbers = []
+
+// for(let num = i; i <=100; i++){
+//     numbers.push(i);
+// }
+
+
+// let reduce = number.reduce(function(sum, item){
+//     return sum + item;
+// })
+
+// console.log(reduce);
+
+// let str = "apple";
+
+// [...str].forEach(function(item, index, array){
+//     console.log(item, index, array);
+// })
+
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let arr;
+// arr = numbers.filter((num) => num > 3);
+// console.log(arr);
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter(word => word.length > 6);
+// console.log(result);
+
+
+
+let fruits1 = ["사과", "딸기", "파인애플", "수박", "참외", "오렌지", "자두", "망고"];
+let fruits2 = ["수박", "사과", "참외", "오렌지", "파인애플", "망고"];
+
+let same = [];
+let diff = [];
+
+same = fruits1.filter((item)=>fruits2.includes(item));
+
+fruits1.forEach(function(item){
+    if(fruits2.includes(item)){
+        same.push(item);
     }
 })
 
-console.log(obj);
